453bb254c8f5b2a88ed4ad9bdeaa649dfda03268
Ivan Plesovskikh <plesovskikh.ivan@gmail.com>
2025-03-21 18:47:43 +0300

    change config

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,10 +2,8 @@ import Database from 'better-sqlite3'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
 
 import * as schema from './schema'
-import path from 'path';
 
 // const sqlite = new Database('sqlite.db')
 
-const sqlitePath = path.join(process.cwd(), "database", "sqlite.db");
-const sqlite = new Database(sqlitePath, { readonly: true });
+const sqlite = new Database('sqlite.db', { readonly: true })
 export const db = drizzle({ client: sqlite, schema })
