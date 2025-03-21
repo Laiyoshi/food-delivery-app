c631b86f92e86584ce2e370faf12162698524848
Ivan Plesovskikh <plesovskikh.ivan@gmail.com>
2025-03-21 18:55:54 +0300

    change config

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,7 +5,8 @@ import * as schema from './schema'
 import path from 'path';
 
 // const sqlite = new Database('sqlite.db')
-const sqlitePath = path.join(process.cwd(), "public", "database", "sqlite.db");
+// const sqlitePath = path.join(process.cwd(), "public", "database", "sqlite.db");
+const sqlite = new Database('public/database/sqlite.db')
 
-const sqlite = new Database(sqlitePath, { readonly: true })
+//const sqlite = new Database(sqlitePath, { readonly: true })
 export const db = drizzle({ client: sqlite, schema })
