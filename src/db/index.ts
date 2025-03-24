605b59dae6a4f62d06c2aef9e3a146091585d75a
Ivan Plesovskikh <plesovskikh.ivan@gmail.com>
2025-03-25 01:36:52 +0300

    add turso

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,21 @@
-import Database from 'better-sqlite3'
-import { drizzle } from 'drizzle-orm/better-sqlite3'
-
+// Локальная разработка
+// import Database from 'better-sqlite3'
+// import { drizzle } from 'drizzle-orm/better-sqlite3'
+// Для Turso в CI
+import { drizzle } from 'drizzle-orm/libsql'
+import { createClient } from '@libsql/client';
+// оставить для всех
 import * as schema from './schema'
 
-const sqlite = new Database('sqlite.db')
 
-export const db = drizzle({ client: sqlite, schema })
+// Локальная разработка
+// const sqlite = new Database('sqlite.db')
+// export const db = drizzle({ client: sqlite, schema })
+
+// Для Turso в CI
+const turso = createClient({
+  url: process.env.DATABASE_URL!,
+  authToken: process.env.DATABASE_AUTH_TOKEN,
+});
+export const db = drizzle({ client: turso, schema })
+
--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,12 +1,16 @@
-import Database from 'better-sqlite3'
-import { drizzle } from 'drizzle-orm/better-sqlite3'
-import { migrate } from 'drizzle-orm/better-sqlite3/migrator'
 
-import * as schema from './schema'
+// import Database from 'better-sqlite3'
+// import { drizzle } from 'drizzle-orm/better-sqlite3'
+// import * as schema from './schema'
+// локальная разработка
+// import { migrate } from 'drizzle-orm/better-sqlite3/migrator'
+import { migrate } from 'drizzle-orm/libsql/migrator'
 
-const sqlite = new Database('sqlite.db')
+import { db } from '.'
 
-export const db = drizzle(sqlite, { schema })
+// const sqlite = new Database('sqlite.db')
+
+// export const db = drizzle(sqlite, { schema })
 
 const main = () => {
   try {
