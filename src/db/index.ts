bd7cba06c5fce0969ae6eeeb5238e582fa8ef026
Dmitriy <hafer144@gmail.com>
2025-04-10 14:45:38 +0300

    change on turso config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,23 +1,23 @@
-// import 'dotenv/config';
+import 'dotenv/config';
 
+import { createClient } from '@libsql/client';
 // Локальная разработка
-import Database from 'better-sqlite3';
-import { drizzle } from 'drizzle-orm/better-sqlite3';
+// import Database from 'better-sqlite3';
+// import { drizzle } from 'drizzle-orm/better-sqlite3';
 
 // Для Turso в CI
-// import { drizzle } from 'drizzle-orm/libsql'
-// import { createClient } from '@libsql/client';
+import { drizzle } from 'drizzle-orm/libsql';
 
 // оставить для всех
 import * as schema from './schema';
 
 // Локальная разработка
-const sqlite = new Database('sqlite.db');
-export const db = drizzle({ client: sqlite, schema });
+// const sqlite = new Database('sqlite.db');
+// export const db = drizzle({ client: sqlite, schema });
 
 // Для Turso в CI
-// const turso = createClient({
-//   url: process.env.DATABASE_URL!,
-//   authToken: process.env.DATABASE_AUTH_TOKEN,
-// });
-// export const db = drizzle({ client: turso, schema })
+const turso = createClient({
+  url: process.env.DATABASE_URL!,
+  authToken: process.env.DATABASE_AUTH_TOKEN,
+});
+export const db = drizzle({ client: turso, schema });
